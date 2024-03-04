b0be9621e93fe4ac60d44ef92088f33def262fa9
AYC <allenyoungchina@outlook.com>
2024-03-04 16:09:51 +0800

    新增首页banner

---
+++ src/views/index/banner.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+let bannerList = ref([
+    {id: 1, img_url: 'banner1.jpg'},
+    {id: 2, img_url: 'banner2.jpg'},
+    {id: 3, img_url: 'banner3.jpg'},
+])
+</script>
+
+<template>
+    <div class="banner">
+        <div class="banner-list">
+            <el-carousel height="600px" arrow="never">
+                <el-carousel-item v-for="item in bannerList" :key="item.id">
+                    <img :src="`/src/assets/images/${item.img_url}`" alt="">
+                </el-carousel-item>
+            </el-carousel>
+            <div class="banner-item">
+            <img src="@/assets/images/footer_bottom.png" alt="">
+        </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
