c472d27c4e48efb5cc2a2c3b570f0573c1319930
AYC <allenyoungchina@outlook.com>
2024-03-04 17:34:35 +0800

    新增获取报价表单

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/css/common.css'
 import './assets/css/index.css'
+import './assets/font/iconfont.css'
 // 导入路由
 // import router from './router/index.js'
 import router from './router'
--- src/views/index/banner.vue
+++ src/views/index/banner.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import { Avatar, PhoneFilled, BrushFilled } from '@element-plus/icons-vue'
 
 let bannerList = ref([
     {id: 1, img_url: 'banner1.jpg'},
@@ -15,6 +16,42 @@ let bannerList = ref([
                 <el-carousel-item v-for="item in bannerList" :key="item.id">
                     <img :src="`/src/assets/images/${item.img_url}`" alt="">
                 </el-carousel-item>
+                <div class="indexForm">
+                    <div class="form_bg">
+                        <div class="form_head">
+                            <b>免费获取</b>家装报价
+                        </div>
+                        <div class="regform">
+                            <el-input
+                                v-model="input2"
+                                style="width: 300px"
+                                size="large"
+                                placeholder="请输入姓名"
+                                :prefix-icon="Avatar"
+                            />
+                            <el-input
+                                v-model="input2"
+                                style="width: 300px"
+                                size="large"
+                                placeholder="请输入电话"
+                                :prefix-icon="PhoneFilled"
+                            />
+                            <el-input
+                                v-model="input2"
+                                style="width: 300px"
+                                size="large"
+                                placeholder="请输入装修面积"
+                                :prefix-icon="BrushFilled"
+                            />
+                            <br>
+                            <el-button color="var(--bgcolor)">获取报价</el-button>
+                        </div>
+                        <div class="privacy">
+                            <input type="checkbox" class="checkbox-type">
+                            <span>已阅读并同意《用户隐私保护协议》</span>
+                        </div>
+                    </div>
+                </div>
             </el-carousel>
             <div class="banner-item">
             <img src="@/assets/images/footer_bottom.png" alt="">
